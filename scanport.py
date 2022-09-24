3dffd72e89a92c5209649a66289fbb6230e1ae8a
Yasir Khairul Malik <yasirkm3071@gmail.com>
2022-09-24 14:29:32 +0700

    ➕ added openports function

--- scanport.py
+++ scanport.py
@@ -2,6 +2,9 @@ import sys
 from ipaddress import ip_address
 from socket import socket, AF_INET, SOCK_STREAM, SHUT_RDWR
 
+UPPER_PORT = 65536
+LOWER_PORT = 0
+
 def port_isopen(ip, port):
     '''
         returns True if port on ip is open
@@ -15,3 +18,16 @@ def port_isopen(ip, port):
 
         except ConnectionRefusedError:
             return False
+
+
+def openports(ip):
+    '''
+        returns a list of open ports of an ip address
+    '''
+    ports = []
+
+    for port in range(LOWER_PORT, UPPER_PORT+1):
+        if port_isopen(ip, port):
+            ports.append(port)
+    
+    return ports
