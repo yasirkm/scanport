96fc8a188929f2ee08b060cf5b2bacc54f416a4b
Yasir Khairul Malik <yasirkm3071@gmail.com>
2022-09-26 17:05:41 +0700

    ➕ added main function

--- scanport.py
+++ scanport.py
@@ -5,6 +5,23 @@ from socket import socket, AF_INET, SOCK_STREAM, SHUT_RDWR
 UPPER_PORT = 90
 LOWER_PORT = 70
 
+def main():
+    if len(sys.argv) < 2:
+        print("Use: python scanport.py {ip address}")
+        sys.exit()
+    
+    ip = sys.argv[1]
+
+    try:
+        ip_address(ip)
+    except ValueError:
+        print("invalid IP address. Example of valid IP: 192.168.0.1 and 2001:db8::1")
+        sys.exit()
+
+    for port in openports(ip):
+        print(port)
+
+
 def port_isopen(ip, port):
     '''
         returns True if port on ip is open
@@ -28,3 +45,6 @@ def openports(ip):
             ports.append(port)
     
     return ports
+
+if __name__ == "__main__":
+    main()
